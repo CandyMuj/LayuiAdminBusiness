d8387a64496e70011960f1c36a47bc2c4ebe067f
Candy <jx003js@gmail.com>
2020-10-20 14:49:38 +0800

    菜单配置使用单独的文件进行维护配置 m.js

---
+++ inc/config/m.js
@@ -0,0 +1,16 @@
+var _menu_ = [
+    {
+        id: 1,
+        pid: 0,
+        node: "试试",
+        url: "http://www.baidu.com",
+        icon: ""
+    },
+    {
+        id: 2,
+        pid: 0,
+        node: "试1试",
+        url: "http://www.baidu.com",
+        icon: ""
+    }
+];
